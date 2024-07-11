f1fcb1318a14ca7f1a8dc1e796624da45536d41c
Satva <satvalite@gmail.com>
2024-07-11 19:32:47 +0530

    random update

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-const amount=12
-if (amount<12){
-    console.log("amount is less than 12")
-}
-else {
-    console.log("amount is greater than 12")
-}
-console.log('hello world')
+
+setInterval(() => {
+    console.log('hi my name is satva')
+    
+}, 1000);

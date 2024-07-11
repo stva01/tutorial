74fcf3459ff6e902ae6aad528263709ffa36171e
Satva <satvalite@gmail.com>
2024-07-12 00:56:17 +0530

    last cimmit idk

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 
 const names=require('./names');
 const sayHi=require('./utils');
-
+const data=require('./alternative')
 console.log(names)
 
  sayHi(names.age)
  sayHi('satva01')
-
+console.log(data)
 

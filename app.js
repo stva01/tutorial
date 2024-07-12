c15b894f0fa0ac8b3ebd3588d219672569f08bad
Satva <satvalite@gmail.com>
2024-07-12 12:00:24 +0530

    mind grenade used

--- app.js
+++ app.js
@@ -2,9 +2,10 @@
 const names=require('./names');
 const sayHi=require('./utils');
 const data=require('./alternative')
-console.log(names)
+require('./mindgrenade')
+// console.log(names)
 
- sayHi(names.age)
- sayHi('satva01')
-console.log(data)
+//  sayHi(names.age)
+//  sayHi('satva01')
+// console.log(data)
 

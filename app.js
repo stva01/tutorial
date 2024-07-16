afd47c00bb55695b02a4e9bbff2d876d20e82b79
Satva <satvalite@gmail.com>
2024-07-16 13:40:27 +0530

    asynch file

--- app.js
+++ app.js
@@ -4,8 +4,23 @@ readFile('./content/first.txt','utf8',(err,result)=>{
         console.log(err)
         return;
     }
-    
+    const first = result
+    readFile('./content/second.txt','utf8',(err,result)=>{
+        if(err){
+            console.log(err)
+            return;
+        }
+    const second = result
+    writeFile('./content/result-async.txt',`Here is the result : ${first}, ${second}`,(err,result)=>{
+        if(err){
+            console.log(err)
+        }
+    })
+
+
 })
+})
+
 
 
 

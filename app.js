654bb2729f2da9ed264e68d33e4e505962acefcd
Satva <satvalite@gmail.com>
2024-08-03 21:31:40 +0530

    promises

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
-const http=require('http')
-const server=http.createServer((res,req)=>{
-    console.log('request event')
-    console.log('hello world')
     
-})
-server.listen(5000,()=>{
-    console.log('listening to port 5000')
-})
